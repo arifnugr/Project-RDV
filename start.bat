5fb519ddd18efad2f261fabb964b375877741796
M. Arif Nugroho <119921362+arifnugr@users.noreply.github.com>
2025-06-23 16:27:11 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,13 +14,8 @@ set MONGODB_HOST=mongodb
 set MONGODB_PORT=27017
 set MONGODB_DATABASE=market_data
 
-<<<<<<< HEAD
 REM Start the services
 docker-compose up -d zookeeper kafka kafka-ui mongodb mongo-express spark-master spark-worker postgres airflow-webserver airflow-scheduler airflow-init kafka-producer kafka-consumer dashboard
-=======
-REM Start all services explicitly in detached mode
-docker-compose up -d zookeeper kafka kafka-ui mongodb mongo-express spark-master spark-worker postgres airflow-init airflow-webserver airflow-scheduler kafka-producer kafka-consumer dashboard
->>>>>>> 1a17f12 (Update)
 
 echo.
 echo Services will be available at:
@@ -30,10 +25,7 @@ echo - Airflow UI: http://localhost:8081 (username: airflow, password: airflow)
 echo - MongoDB UI: http://localhost:8082
 echo - Dashboard UI: http://localhost:8501
 echo.
-<<<<<<< HEAD
 echo To stop the services, run: docker-compose down
 echo.
-=======
 echo Press Ctrl+C to stop all services
 echo.
->>>>>>> 1a17f12 (Update)
